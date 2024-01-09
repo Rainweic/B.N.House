0a957c95b8a8c55396392b4abb633baf29baecf8
cu <861514706@qq.com>
2024-01-09 19:33:04 +0800

    env

---
+++ demo.py
@@ -0,0 +1,12 @@
+import gymnasium as gym
+
+# 创建环境
+env = gym.make("LunarLander-v2", render_mode="human")
+observation, info = env.reset()
+
+# 开始游戏
+while True:
+    action = env.action_space.sample()  # 随机选择动作
+    observation, reward, done, info = env.step(action)  # 执行动作并获取反馈
+    if done:  # 游戏结束
+        break
