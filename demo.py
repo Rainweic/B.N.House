eb22efbb39468de0cefb0f18f51ff4e63943d684
Rainweic <861514706@qq.com>
2024-01-10 11:29:59 +0800

    update

--- demo.py
+++ demo.py
@@ -4,16 +4,14 @@ import gymnasium as gym
 env = gym.make("LunarLander-v2", render_mode="human")
 observation, info = env.reset()
 
-# 开始游戏
-while True:
-    action = env.action_space.sample()  # 随机选择动作
-    observation, reward, done, info = env.step(action)  # 执行动作并获取反馈
-    if done:  # 游戏结束
-        break
-    
-    # 显示游戏画面
-    env.render()
-    
 
-# 关闭环境
+while True:
+
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+
 env.close()
