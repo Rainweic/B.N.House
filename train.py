3db3b0841abeefe0d4c2368c2f22aceee402f272
cu <861514706@qq.com>
2024-01-22 11:14:56 +0800

    [update]

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ from tianshou.data import Collector, VectorReplayBuffer
 from torch.utils.tensorboard import SummaryWriter
 from tianshou.utils import TensorboardLogger
 from tianshou.trainer import OffpolicyTrainer
+from tianshou.policy import BasePolicy
+from tianshou.data.batch import Batch
 
 from configs import read_config
 from model_zoo import MODEL_ZOO
@@ -42,16 +44,16 @@ def train(args):
         train_envs = gym.make(config.env_name)
 
     if hasattr(config, "test_num"):
-        test_envs = SubprocVectorEnv([lambda: gym.make(config.env_name) for _ in range(config.test_num)])
+        eval_envs = SubprocVectorEnv([lambda: gym.make(config.env_name) for _ in range(config.test_num)])
     else:
-        test_envs = gym.make(config.env_name)
+        eval_envs = gym.make(config.env_name)
 
     # set seed
     if hasattr(config, "random_seed"):
         np.random.seed(config.random_seed)
         torch.manual_seed(config.random_seed)
         train_envs.seed(config.random_seed)
-        test_envs.seed(config.random_seed)
+        eval_envs.seed(config.random_seed)
 
     # Model
     model = MODEL_ZOO[config.model](config=config)
@@ -59,13 +61,13 @@ def train(args):
     # GPU or CPU
     use_cuda = torch.cuda.is_available()
     if use_cuda:
-        model = model.to("cuda")
+        model.to("cuda")
 
     # Optimizer
     optim = OPTIMIZER[config.optimizer](model.parameters(), lr=config.lr)
 
     # Policy
-    policy = POLICY_ZOO[config.policy](
+    policy: BasePolicy = POLICY_ZOO[config.policy](
         model=model,
         optim=optim,
         action_space=tmp_env.action_space,
@@ -81,7 +83,7 @@ def train(args):
         VectorReplayBuffer(config.buffer_size, len(train_envs)),
         exploration_noise=True,
     )
-    test_collector = Collector(policy, test_envs, exploration_noise=True)
+    test_collector = Collector(policy, eval_envs, exploration_noise=True)
 
     # Log
     log_path = os.path.join("./logs", config.env_name, config.model)
@@ -89,7 +91,11 @@ def train(args):
     logger = TensorboardLogger(writer)
 
     def save_best_fn(policy):
-        torch.save(policy.state_dict(), os.path.join(log_path, "policy.pth"))
+        if hasattr(config, "save_model_name"):
+            save_name = config.save_model_name
+        else:
+            save_name = "policy.pth"
+        torch.save(policy.state_dict(), os.path.join(log_path, save_name))
 
     def stop_fn(mean_rewards):
         return mean_rewards >= tmp_env.spec.reward_threshold
@@ -118,6 +124,32 @@ def train(args):
         save_best_fn=save_best_fn,
         logger=logger,
     ).run()
+
+    # test
+    if stop_fn(result.get("best_reward")):
+
+        if hasattr(config, "save_model_name"):
+            save_name = config.save_model_name
+        else:
+            save_name = "policy.pth"
+        policy.load_state_dict(torch.load(os.path.join(log_path, save_name)))
+
+        test_env = gym.make(config.env_name)
+        obs, info = test_env.reset()
+
+        act = policy(Batch(obs=obs[np.newaxis, :], info={})).act.item()
+        act = policy.map_action(act)
+
+        while True:
+            obs, reward, terminated, truncated, info = test_env.step(act)
+
+            if terminated or truncated:
+                break
+
+            act = policy(Batch(obs=obs[np.newaxis, :], info={})).act.item()
+            act = policy.map_action(act)
+
+        test_env.close()
 
 
 if __name__ == "__main__":
