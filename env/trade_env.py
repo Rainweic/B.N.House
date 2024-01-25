b6403577a75007dbd64eab1a3a18c4e071d16e6d
Rainweic <861514706@qq.com>
2024-01-25 10:54:42 +0800

    [update]

--- env/trade_env.py
+++ env/trade_env.py
@@ -1,4 +1,5 @@
 from gymnasium import Env
+from gymnasium import spaces
 
 
 class TradeEnv(Env):
@@ -8,4 +9,19 @@ class TradeEnv(Env):
     def __init__(self) -> None:
         super().__init__()
 
-        self.action_space = ...
+        # Action dict:
+        # 0: 做空  1: 做多  2: 保持当前仓位
+        self.action_space = spaces.Discrete()
+        self._action_to_trade = {
+            0: ...,
+            1: ...,
+            2: ...,
+        }
+
+        self.observation_space = ...
+
+    def reset(self, *, seed, options):
+        return super().reset(seed=seed, options=options)
+
+    def step(self, action):
+        return super().step(action)
