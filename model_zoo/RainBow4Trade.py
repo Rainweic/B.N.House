9d8d62ea1c993c204f083b4db6fb070654bca89a
Rainweic <861514706@qq.com>
2024-02-18 11:00:58 +0800

    [update] 更新命名

--- model_zoo/RainBow4Trade.py
+++ model_zoo/RainBow4Trade.py
@@ -1,1 +1,7 @@
-from atari_network import Rainbow
+from torch import nn
+
+
+class RainBow4Trade(nn.Module):
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RainBow4Trade, self).__init__()
